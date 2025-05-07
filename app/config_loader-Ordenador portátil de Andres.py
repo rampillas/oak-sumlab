86f47ce2a8c608284f7f73371299eb4e9adc72e1
rampillas <andresrgutierrez@gmail.com>
2025-05-07 17:31:49 +0200

    c

--- app/config_loader-Ordenador portátil de Andres.py
+++ app/config_loader-Ordenador portátil de Andres.py
@@ -2,11 +2,11 @@
 import yaml
 import os
 
-config_path = os.getenv("CONFIG_PATH", "config.yaml")
+#config_path = os.getenv("CONFIG_PATH", "config.yaml")
 
 
 def load_config(config_path="../config/config.yaml"):
-    config_path = os.getenv("CONFIG_PATH", "config.yaml")
+    #config_path = os.getenv("CONFIG_PATH", "config.yaml")
     """Loads configuration from a YAML file."""
     # Ensure config path is absolute for container environments
     if not os.path.isabs(config_path):
